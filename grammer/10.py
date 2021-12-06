238b38300127738d44ba9fd6eefcb068dc3bbbd1
UNOHOME\User <yhlee@oa-world.com>
2021-12-06 22:50:08 +0900

    no message

--- grammer/10.py
+++ grammer/10.py
@@ -14,6 +14,11 @@
 
 
 from travel import *
-trip_to = vietnam.VietnamPackage()
-trip_to = thailand.ThailandPackage()
-trip_to.detail()
+# trip_to = vietnam.VietnamPackage()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
+
+import inspect
+import random
+# print(inspect.getfile(random))
+print(inspect.getfile(thailand))
--- grammer/travel/thailand.py
+++ grammer/travel/thailand.py
@@ -1,3 +1,11 @@
 class ThailandPackage:
     def detail(self):
         print("[태국3박5일] 야시장 투저")
+
+if __name__ == "__main__":
+    print("Thailand 모듈을 직접 실행함")
+    print("이 문장은 모듈을 직접 실행할 때만 실행되요")
+    trip_to = ThailandPackage()
+    trip_to.detail()
+else:
+    print("Thailand  외부에서 모듈 호출")
